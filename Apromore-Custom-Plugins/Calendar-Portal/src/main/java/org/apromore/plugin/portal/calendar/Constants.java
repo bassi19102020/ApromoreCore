e79ed2554d42f49f60f72c0eeb5da399106bae46
Ivo Widjaja <dreamind@gmail.com>
2020-10-29 16:05:13 +1100

    AP-2460 MVC-based Calendar Plugin

--- Apromore-Custom-Plugins/Calendar-Portal/src/main/java/org/apromore/plugin/portal/calendar/Constants.java
+++ Apromore-Custom-Plugins/Calendar-Portal/src/main/java/org/apromore/plugin/portal/calendar/Constants.java
@@ -8,40 +8,30 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
-package org.apromore.calendar.model;
+package org.apromore.plugin.portal.calendar;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import lombok.Data;
-import lombok.ToString;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 
-@Data
-@ToString
-public class DurationModel {
+import org.apromore.commons.datetime.TimeUtils;
 
-  Duration duration = Duration.ZERO;
-  Duration durationMin = Duration.ZERO;
-  Duration durationMax = Duration.ZERO;
-  Duration durationMean = Duration.ZERO;
-  Duration durationMedian = Duration.ZERO;
-  Duration durationSum = Duration.ZERO;
+public abstract class Constants {
 
-  public void setAll(Duration totalDuration) {
-    duration =
-        durationMin = durationMax = durationMean = durationMedian = durationSum = totalDuration;
-
-  }
-
-
+    // Reference dummy to convert startTime and endTime to Date
+    public static final LocalDate LOCAL_DATE_REF = Instant
+            .ofEpochMilli( 0L )
+            .atOffset(ZoneOffset.UTC)
+            .toLocalDate();
 }
--- Apromore-Custom-Plugins/Calendar-Portal/src/main/java/org/apromore/plugin/portal/calendar/Zone.java
+++ Apromore-Custom-Plugins/Calendar-Portal/src/main/java/org/apromore/plugin/portal/calendar/Zone.java
@@ -8,40 +8,29 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
-package org.apromore.calendar.model;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import lombok.Data;
-import lombok.ToString;
+package org.apromore.plugin.portal.calendar;
 
-@Data
-@ToString
-public class DurationModel {
+import lombok.Getter;
+import lombok.Setter;
 
-  Duration duration = Duration.ZERO;
-  Duration durationMin = Duration.ZERO;
-  Duration durationMax = Duration.ZERO;
-  Duration durationMean = Duration.ZERO;
-  Duration durationMedian = Duration.ZERO;
-  Duration durationSum = Duration.ZERO;
+public class Zone {
+    @Getter @Setter private String id;
+    @Getter @Setter private String displayName;
 
-  public void setAll(Duration totalDuration) {
-    duration =
-        durationMin = durationMax = durationMean = durationMedian = durationSum = totalDuration;
-
-  }
-
-
+    public Zone(String id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
 }
